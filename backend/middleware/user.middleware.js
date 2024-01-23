3bfe24e10e542f7322f58a60d9606b9e6c717934
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-01-23 20:50:50 +0530

    Added the auth middleware

---
+++ backend/middleware/user.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+import { JWT_SECRET } from "../config.js";
+config()
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({
+            success: false,
+            message: "Invalid Header"
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if (decoded.userId) {
+            req.userId = decoded.userId;
+            next();
+        }
+
+    } catch (err) {
+        return res.status(403).json({
+            success: false,
+            message: "Could not verify the token"
+        });
+    }
+};
+
+export {
+    authMiddleware
+}
