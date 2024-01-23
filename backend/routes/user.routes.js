7147a59a278d2af796cd8da2b8fefdec17837871
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-01-23 20:49:36 +0530

    Added the userModel and userRoutes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { getUserDetails, signin, signup, updateDetails } from '../controllers/user.controller.js';
+import { authMiddleware } from '../middleware/user.middleware.js';
+
+const router = express.Router()
+
+router.post('/signup', signup);
+router.post('/signin', signin);
+router.put('/updateDetails', authMiddleware, updateDetails);
+router.get("/getDetails", authMiddleware, getUserDetails);
+
+export default router;
