20fdc2b483268499f8960d2eb915668d70902232
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-02-03 20:49:36 +0530

    Final commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { Signin } from "./Pages/Signin";
 import { Dashboard } from "./Pages/Dashboard";
 import { SendMoney } from "./Pages/SendMoney";
 import PageNotFound from "./Pages/PageNotFound";
+import Home from "./Pages/Home";
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { Appbar } from "../components/Appbar"
 
 function Home() {
     return (
-        <div>Home</div>
+        <>
+            <Appbar />
+            <div className="h-[90vh] flex flex-col justify-center items-center">
+                <h1 className="text-5xl text-[#0095ff] ">Welcome</h1>
+                <p className="text-3xl ">PayPulse</p>
+                <div className="flex">
+                    <Link to={"/signup"} className="border px-4 py-2 mt-4 mr-3 bg-gray-400 rounded-lg">Signup</Link>
+                    <Link to={"/signin"} className="border px-4 py-2 mt-4 bg-gray-400 rounded-lg">Signin</Link>
+                </div>
+            </div>
+        </>
     )
 }
 
